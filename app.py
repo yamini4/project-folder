a6b3b645d5dd283b815fe47b14b775e51c2342ce
yamini4 <yamini4.g@gmail.com>
2026-02-11 19:56:44 +0530

    updating title

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 
 # ---------------- PAGE CONFIG ----------------
 st.set_page_config(page_title="Adult Income Classification", layout="wide")
-st.title("📊 Adult Income – Model Evaluation App")
+st.title("📊 Adult Income – Model Evaluation App - ML Classification Models")
 
 # ---------------- LOAD MODELS ----------------
 MODEL_MAP = {
